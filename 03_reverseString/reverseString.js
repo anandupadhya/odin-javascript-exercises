902cefbc23b4ffce6e899e845f6f03d05813a54c
Anand Upadhya <anand.upadhya@yahoo.com>
2022-03-17 06:29:54 -0700

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const length = string.length
+    const chars = string.split("")
+    let reverseChars = []
+    for (i = length - 1; i >= 0; i--) {
+        reverseChars.push(chars[i])
+    }
+    return reverseChars.join('')
 };
 
+// console.log(reverseString('hello there'))
 // Do not edit below this line
 module.exports = reverseString;
